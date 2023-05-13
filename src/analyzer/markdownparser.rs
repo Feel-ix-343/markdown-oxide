b41c800646de3efcdd0d0b9b857d0f3dd7a989e6
Felix Zeller <felixazeller@gmail.com>
2023-05-12 23:01:00 -0400

    Adding header queries

--- src/analyzer/markdownparser.rs
+++ src/analyzer/markdownparser.rs
@@ -1,33 +1,32 @@
 use itertools::Itertools;
 use tree_sitter::{Query, QueryCursor};
-use tree_sitter_md::{MarkdownParser, inline_language};
+use tree_sitter_md::{MarkdownParser, inline_language, language};
 
 
-pub struct MarkdownLinkParser {
+pub struct MarkdownNodeParser {
     parser: MarkdownParser,
 }
 
-impl MarkdownLinkParser {
-    pub fn new() -> MarkdownLinkParser {
-        MarkdownLinkParser {
+impl MarkdownNodeParser {
+    pub fn new() -> MarkdownNodeParser {
+        MarkdownNodeParser {
             parser: MarkdownParser::default()
         }
     }
 
-    pub fn links_for_file<'a>(&mut self, source_code: &'a [u8]) -> Vec<&'a str> {
+    fn query_matches_inline<'a>(&mut self, source_code: &'a [u8], query: &str) -> Vec<&'a str> {
         let parser = &mut self.parser;
 
         let tree = parser.parse(source_code, None).unwrap();
         // let language = language();
         let inline_language = inline_language();
 
-        // let block_tree = tree.block_tree();
         let inline_trees = tree.inline_trees();
 
         // Finding links in the files
 
         // Execute a treesitter query for finding the links from a file
-        let query = Query::new(inline_language, "(link_text) @link;").unwrap();
+        let query = Query::new(inline_language, query).unwrap();
 
         let mut query_cursor = QueryCursor::new();
         let text_provider: &[u8] = &[];
@@ -41,5 +40,47 @@ impl MarkdownLinkParser {
             .collect(); // TODO: I still want to refactor this more
 
         return links
+    }
+
+    fn query_matches_block<'a>(&mut self, source_code: &'a [u8], query: &str) -> Vec<&'a str> {
+        let parser = &mut self.parser;
+
+        let tree = parser.parse(source_code, None).unwrap();
+        // let language = language();
+        let language = language();
+
+        let block_tree = tree.block_tree();
+
+        // Finding links in the files
+
+        // Execute a treesitter query for finding the links from a file
+        let query = Query::new(language, query).unwrap();
+
+        let mut query_cursor = QueryCursor::new();
+        let text_provider: &[u8] = &[];
+
+        let text = query_cursor
+            .matches(&query, block_tree.root_node(), text_provider)
+            .flat_map(|m| m.captures)
+            .map(|c| c.node.utf8_text(source_code).unwrap())
+            .collect_vec();
+
+        // wow what a bad bug! Don't do this! All of the matches will have the ranges
+        // let matches = query_cursor.matches(&query, block_tree.root_node(), text_provider).collect_vec();
+        // println!("matches: {:?}", matches);
+
+        return text
+    }
+
+    pub fn links_for_file<'a>(&mut self, source_code: &'a [u8]) -> Vec<&'a str> {
+        return self.query_matches_inline(source_code, "(link_text) @link;");
+    }
+
+
+    pub fn headings_for_file<'a>(&mut self, source_code: &'a [u8]) -> Vec<&'a str> {
+        let headings = self.query_matches_block(source_code, "(atx_heading heading_content: (inline) @link);");
+        let trimmed_headings = headings.iter().map(|s| s.trim_start()).collect_vec();
+        println!("headings: {:?}", trimmed_headings);
+        return trimmed_headings
     }
 }
