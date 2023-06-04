c41542d0b64ab3ce528eadf4a75efedb7656507b
Felix Zeller <felixazeller@gmail.com>
2023-06-03 21:28:43 -0400

    Parsing Files, Headings, and Paragraphs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,41 @@
-use std::path::PathBuf;
-
-use analyzer::{analyzer::Analyzer};
-use itertools::Itertools;
-
-use analyzer::nodes::*;
-use analyzer::graph::Node;
-
+// use std::path::PathBuf;
+// 
+// use analyzer::{analyzer::Analyzer};
+// use itertools::Itertools;
+// 
+// use analyzer::nodes::*;
+// use analyzer::graph::Node;
+// 
+// mod analyzer;
+// mod lsp;
 mod analyzer;
-mod lsp;
+
+use analyzer::vault_parser::{self, parse_vault};
 
 fn main() {
-    let analyzer = Analyzer::new("/home/felix/Notes");
-    let graph = analyzer.construct_graph();
 
-    // test 
-    let current_file_path: PathBuf = PathBuf::from("/home/felix/Notes/Obsidian Markdown Language Server.md");
-    let md_file: &MDFile = analyzer.files.get(&current_file_path).unwrap();
-    let incoming_links = md_file.incoming(&graph).unwrap();
-    let names = incoming_links.iter().map(|f| f.name()).collect_vec();
-    println!("Incoming {:#?}", names);
-    let outgoing_links = md_file.outgoing(&graph).unwrap();
-    let names = outgoing_links.iter().map(|f| f.name()).collect_vec();
-    println!("Outgoing {:#?}", names);
+    parse_vault("/home/felix/Notes").unwrap();
 
-    // Test heading
 
-    let heading = md_file.headings.iter().find(|h| h.ref_name == "Obsidian Markdown Language Server#Development").unwrap();
-    let heading_incoming = heading.incoming(&graph).unwrap();
-    let names = heading_incoming.iter().map(|f| f.name()).collect_vec();
-    println!("Heading Incoming {:#?}", names);
+    // let analyzer = Analyzer::new("/home/felix/Notes");
+    // let graph = analyzer.construct_graph();
+
+    // // test 
+    // let current_file_path: PathBuf = PathBuf::from("/home/felix/Notes/Obsidian Markdown Language Server.md");
+    // let md_file: &MDFile = analyzer.files.get(&current_file_path).unwrap();
+    // let incoming_links = md_file.incoming(&graph).unwrap();
+    // let names = incoming_links.iter().map(|f| f.name()).collect_vec();
+    // println!("Incoming {:#?}", names);
+    // let outgoing_links = md_file.outgoing(&graph).unwrap();
+    // let names = outgoing_links.iter().map(|f| f.name()).collect_vec();
+    // println!("Outgoing {:#?}", names);
+
+    // // Test heading
+
+    // let heading = md_file.headings.iter().find(|h| h.ref_name == "Obsidian Markdown Language Server#Development").unwrap();
+    // let heading_incoming = heading.incoming(&graph).unwrap();
+    // let names = heading_incoming.iter().map(|f| f.name()).collect_vec();
+    // println!("Heading Incoming {:#?}", names);
 }
 
 
