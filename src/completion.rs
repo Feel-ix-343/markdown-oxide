f72724c652ec1fb5a77c0acb5c2cdd69f1149cf0
Felix Zeller <felixazeller@gmail.com>
2023-12-20 10:59:33 -0500

    Semi-support for completions

--- src/completion.rs
+++ src/completion.rs
@@ -1,10 +1,10 @@
 use itertools::Itertools;
-use tower_lsp::lsp_types::{CompletionResponse, CompletionParams, CompletionTriggerKind, CompletionContext, CompletionItem};
+use tower_lsp::lsp_types::{CompletionResponse, CompletionParams, CompletionContext, CompletionItem, CompletionItemKind};
 
 use crate::vault::{Vault, Referenceable};
 
-pub fn get_completions(vault: &Vault, params: CompletionParams) -> Option<CompletionResponse> {
-    return match params {
+pub fn get_completions(vault: &Vault, params: &CompletionParams) -> Option<CompletionResponse> {
+    match params {
         CompletionParams { context: Some(CompletionContext { trigger_character: Some(character), .. }), .. } if character == "#" => {
             // Initial Tag completion
             let all_tags = vault.select_linkable_nodes()
@@ -15,6 +15,31 @@ pub fn get_completions(vault: &Vault, params: CompletionParams) -> Option<Comple
                 all_tags.map(|tag| tag.get_refname(&vault.root_dir()).map(|root| CompletionItem {label: root, ..Default::default()})).flatten().unique_by(|c| c.label.to_owned()).collect_vec()
             ))
         },
-        _ => None
+        CompletionParams { text_document_position, .. } => {
+
+            let Ok(path) = text_document_position.text_document.uri.to_file_path() else {
+                return None;
+            };
+
+            let line = text_document_position.position.line as usize;
+            let character = text_document_position.position.character as usize;
+
+            let selected_line = vault.select_line(&path.to_path_buf(), line)?;
+
+            if selected_line.get(character-2..character) == Some(&vec!['[', '[']) { // we have a link
+
+                let all_tags = vault.select_linkable_nodes()
+                    .into_iter()
+                    .filter(|referenceable| !matches!(referenceable, Referenceable::Tag(_, _)));
+
+                return Some(CompletionResponse::Array(
+                    all_tags.map(|tag| tag.get_refname(&vault.root_dir()).map(|root| CompletionItem { kind: Some(CompletionItemKind::REFERENCE), label: root.replace(" ", "_"), insert_text: Some(root), ..Default::default()})).flatten().unique_by(|c| c.label.to_owned()).collect_vec()
+                ))
+            } else {
+                return None
+            }
+        }
     }
 }
+
+
