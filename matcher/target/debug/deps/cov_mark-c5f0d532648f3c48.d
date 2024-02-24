ab13e12af3336b37f31ee118396b5647633fa5e3
Felix Zeller <felixazeller@gmail.com>
2024-02-24 13:42:06 -0500

    Remove matcher target folder

--- matcher/target/debug/deps/cov_mark-c5f0d532648f3c48.d
+++
@@ -1,5 +0,0 @@
-/home/felix/coding/LargerIdeas/ObsidianLS/obsidian-ls/matcher/target/debug/deps/libcov_mark-c5f0d532648f3c48.rmeta: /home/felix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cov-mark-1.1.0/src/lib.rs
-
-/home/felix/coding/LargerIdeas/ObsidianLS/obsidian-ls/matcher/target/debug/deps/cov_mark-c5f0d532648f3c48.d: /home/felix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cov-mark-1.1.0/src/lib.rs
-
-/home/felix/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cov-mark-1.1.0/src/lib.rs:
