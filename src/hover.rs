d8fecae3ead05d7e6fb8f383088acde2af247b02
Felix Zeller <felixazeller@gmail.com>
2024-03-02 00:43:53 -0500

    feat: add hover for referenceables

--- src/hover.rs
+++ src/hover.rs
@@ -2,21 +2,21 @@ use std::path::Path;
 
 use tower_lsp::lsp_types::{Hover, HoverContents, HoverParams};
 
-use crate::{ui::preview_reference, vault::Vault};
+use crate::{ui::{preview_reference, preview_referenceable}, vault::Vault};
 
 pub fn hover(vault: &Vault, params: &HoverParams, path: &Path) -> Option<Hover> {
     let cursor_position = params.text_document_position_params.position;
 
-    let links = vault.select_references(Some(path))?;
-    let (refpath, reference) = links.iter().find(|&l| {
-        l.1.data().range.start.line <= cursor_position.line
-            && l.1.data().range.end.line >= cursor_position.line
-            && l.1.data().range.start.character <= cursor_position.character
-            && l.1.data().range.end.character >= cursor_position.character
-    })?;
 
-    preview_reference(vault, refpath, reference).map(|markup| Hover {
-        contents: HoverContents::Markup(markup),
-        range: None,
-    })
+    match (vault.select_reference_at_position(path, cursor_position), vault.select_referenceable_at_position(path, cursor_position)) {
+        (Some(reference), _) => preview_reference(vault, path, reference).map(|markup| Hover {
+            contents: HoverContents::Markup(markup),
+            range: None,
+        }),
+        (None, Some(referenceable)) => preview_referenceable(vault, &referenceable).map(|markup| Hover {
+            contents: HoverContents::Markup(markup),
+            range: None,
+        }),
+        _ => None,
+    }
 }
