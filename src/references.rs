987793e80467550072c87c40acebd9ac09f9d657
Felix Zeller <felixazeller@gmail.com>
2024-03-07 22:27:42 -0500

    Fix reference references bug

--- src/references.rs
+++ src/references.rs
@@ -6,23 +6,25 @@ use tower_lsp::lsp_types::{Location, Position, Url};
 use crate::vault::{Reference, Vault};
 
 pub fn references(vault: &Vault, cursor_position: Position, path: &Path) -> Option<Vec<Location>> {
-    let references: Vec<(&Path, &Reference)> = vault
-        .select_referenceable_at_position(path, cursor_position)
-        .and_then(|referenceable| vault.select_references_for_referenceable(&referenceable))
-        .or(vault
-            .select_reference_at_position(path, cursor_position)
-            .map(|reference| {
-                let referenceables = vault.select_referenceables_for_reference(reference, path);
-                let references = referenceables
-                    .into_iter()
-                    .filter_map(|referenceable| {
-                        vault.select_references_for_referenceable(&referenceable)
-                    }) // drop the Nones on the options
-                    .flatten()
-                    .collect_vec();
 
-                references
-            }))?;
+    let references: Vec<(&Path, &Reference)> = vault
+        .select_reference_at_position(path, cursor_position)
+        .map(|reference| {
+            let referenceables = vault.select_referenceables_for_reference(reference, path);
+            let references = referenceables
+                .into_iter()
+                .filter_map(|referenceable| {
+                    vault.select_references_for_referenceable(&referenceable)
+                }) // drop the Nones on the options
+                .flatten()
+                .collect_vec();
+
+            references
+        })
+            .or(vault
+            .select_referenceable_at_position(path, cursor_position)
+            .and_then(|referenceable| vault.select_references_for_referenceable(&referenceable))
+        )?;
 
     Some(
         references
