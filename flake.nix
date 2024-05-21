c324728fe175684f0819c1f82212995cc03933b5
Felix Zeller <felixazeller@gmail.com>
2024-05-21 16:14:36 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            rust-bin.nightly."2024-05-20".default
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+
+            zsh
+          '';
+        };
+      }
+    );
+}
